572103f721abf881799abede0a40f80d8e3cf659
Sandeep <mailsandeep003@gmail.com>
2023-10-18 13:16:20 -0400

    created azure function api

--- frontend/main.js
+++ frontend/main.js
@@ -1,8 +1,8 @@
 window.addEventListener('DOMContentLoaded', (event) => {
   getVisitCount();
-})
+});
 
-const functionApi = '';
+const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
   let count = 30;
@@ -12,11 +12,12 @@ const getVisitCount = () => {
     })
     .then((response) => {
       console.log('Api called');
-      count = response.count;
+      count = response.counter;
       document.getElementById('counter').innerText = count;
+      console.log(response);
     })
     .catch(function (error) {
       console.log(error);
     });
-  return.count;
+  return count;
 };
